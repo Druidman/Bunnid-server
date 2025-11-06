ac99f20b2349b22feb54290acfa422a560b93dfe
Mateusz Kowalski <towermanxxl@gmail.com>
2025-11-06 19:02:19 +0100

    added conversation endpoints

--- db/tables/conversations.py
+++ db/tables/conversations.py
@@ -6,31 +6,33 @@ def get_conversation(convId: int, db: sqlite3.Cursor) -> dict:
     if (convId <= -1):
         return DB_RESULT(False, "wrong convId")
     
-    db.execute("SELECT title FROM conversations WHERE conversationId=:convId", {
+    db.execute("SELECT title, id FROM conversations WHERE conversationId=:convId LIMIT 1", {
         "convId": convId
     })
     rows = db.fetchone()
+    if (not rows):
+        return DB_RESULT(False, "No such conversation")
     return DB_RESULT(True, rows)
 
 @dbFunction
-def get_conversation_by_name(name: str, db: sqlite3.Cursor) -> dict:
-    if (name == ""):
-        return DB_RESULT(False, "wrong conv name")
+def get_conversation_by_title(title: str, db: sqlite3.Cursor) -> dict:
+    if (title == ""):
+        return DB_RESULT(False, "wrong conv ntitleame")
     
-    db.execute("SELECT conversationId FROM conversations WHERE name=:name", {
-        "name": name
+    db.execute("SELECT conversationId FROM conversations WHERE title=:title", {
+        "title": title
     })
     rows = db.fetchone()
     return DB_RESULT(True, rows)
 
 
 @dbFunction
-def add_conversation(name: str, db: sqlite3.Cursor) -> dict:
-    if (name==""):
-        return DB_RESULT(False, "wrong name")
+def add_conversation(title: str, db: sqlite3.Cursor) -> dict:
+    if (title==""):
+        return DB_RESULT(False, "wrong title")
     
-    db.execute("INSERT INTO conversations(name) VALUES(:name)", {
-        "name": name
+    db.execute("INSERT INTO conversations(title) VALUES(:title)", {
+        "title": title
     })
     db.connection.commit()
     return DB_RESULT(True, True)
--- httpServer/blueprints/services/conversation.py
+++ httpServer/blueprints/services/conversation.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 from server.httpServer.auth import userSession
+from server.db.tables.conversations import get_conversation, add_conversation
+from server.db.tables.messages import get_messages, add_message
 
 from server import globals
 
@@ -13,16 +15,70 @@ def conversationMain():
 
 @conversation_bp.route("/send", methods=["POST"])
 @userSession
-def conversationMain(): pass
+def conversationSendMsg():
+    try:
+        conversationId = request.json.get("conversationId")
+        msgContent = request.json.get("msgContent")
+        userId = request.json.get("userId")
+    except:
+        return globals.errors["NO_ARGS"]
+    
+    result = add_message(convId=conversationId, userId=userId, content=msgContent, db=globals.dbConn.cursor())
 
+    if (not result["STATUS"]):
+        return globals.API_RESPONSE(False, result["MSG"])
+    
+    return globals.API_RESPONSE(True, result["MSG"])
 
-@conversation_bp.route("/get", methods=["POST"])
+@conversation_bp.route("/getMessages", methods=["POST"])
 @userSession
-def conversationMain():
+def conversationGetMessages():
     try:
         conversationId = request.json.get("conversationId")
     except:
         return globals.errors["NO_ARGS"]
+    
+    result = get_messages(convId=conversationId, db=globals.dbConn.cursor())
+
+    if (not result["STATUS"]):
+        return globals.API_RESPONSE(False, result["MSG"])
+    
+    return globals.API_RESPONSE(True, result["MSG"])
+
+
+@conversation_bp.route("/create", methods=["POST"])
+@userSession
+def conversationCreate():
+    try:
+        conversationTitle = request.json.get("conversationTitle")
+    except:
+        return globals.errors["NO_ARGS"]
+    
+    result = add_conversation(title=conversationTitle, db=globals.dbConn.cursor())
+
+    if (not result["STATUS"]):
+        return globals.API_RESPONSE(False, result["MSG"])
+    
+    return globals.API_RESPONSE(True, result["MSG"])
+    
+@conversation_bp.route("/get", methods=["POST"])
+@userSession
+def conversationGet():
+    try:
+        conversationId = request.json.get("conversationId")
+    except:
+        return globals.errors["NO_ARGS"]
+    
+    conversation = get_conversation(convId=conversationId, db=globals.dbConn.cursor())
+
+    if (not conversation["STATUS"]):
+        return globals.API_RESPONSE(False, conversation["MSG"])
+    
+    return globals.API_RESPONSE(True, conversation["MSG"])
+
+
+    
+
     
     
 
