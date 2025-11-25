47ba28873ae083a0b9bb8ea8077eaf68b5b72d41
Bober <towermanxxl@gmail.com>
2025-11-25 21:23:35 +0100

    testing different ports

--- server/httpServer/main.py
+++ server/httpServer/main.py
@@ -16,5 +16,5 @@ def run_http_server():
     app.register_blueprint(session_bp, url_prefix="/api/session")
     app.register_blueprint(docs_bp, url_prefix="/api/docs")
     app.register_blueprint(service_bp, url_prefix="/api/service")
-    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=5000)
 
