a78bfe9abc584c7b663749c65ec720650fb2b3de
Bober <towermanxxl@gmail.com>
2025-11-29 20:10:54 +0100

    auth thru msg

--- server/httpServer/routes/websocket.py
+++ server/httpServer/routes/websocket.py
@@ -3,19 +3,42 @@ from fastapi import APIRouter, WebSocket, Query, WebSocketDisconnect, WebSocketE
 from server.httpServer.auth.rt_session import check_if_valid_rts_token
 
 from server.ws.objects.client import Client
-import asyncio
+import server.globals as globals
 
 
 websocket_router = APIRouter(prefix="/ws")
 
+class NotAuthorized(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+    
+    def __str__(self) -> str:
+        return f"Client not authorized to use websocket connection"
+    
+async def authenticate_client(websocket: WebSocket) -> None:
+    try:
+        authData = await websocket.receive_json()
+
+        if not globals.validateObject(authData, ["event", "data", "requestId", "error"]):
+            raise NotAuthorized
+        if not globals.validateObject(authData["data"], ["RTSToken"]):
+            raise NotAuthorized
+        token = authData["data"]["RTSToken"]
+        
+        if (not await check_if_valid_rts_token(token)):
+            raise NotAuthorized
+        
+    except:
+        raise
+
 @websocket_router.websocket("/")
-async def websocketMain(websocket: WebSocket, token: str = Query(...)) -> str:
+async def websocketMain(websocket: WebSocket) -> str:
 
     try:
         await websocket.accept()
-        if (not await check_if_valid_rts_token(token)):
-            await websocket.close()
-        
+        # Auth
+        await authenticate_client()
+    
         client = Client(websocket)
      
         while True:
@@ -27,6 +50,9 @@ async def websocketMain(websocket: WebSocket, token: str = Query(...)) -> str:
         print("websocket disconnected")
     except WebSocketException:
         print("websocket exception")
+        await websocket.close()
+    except NotAuthorized as e:
+        print(e.__str__())
         await websocket.close()
     except Exception as e:
         print(f"Exception in websocketMain: {e}")
