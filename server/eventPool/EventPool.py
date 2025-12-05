be4f4a0390c6427e1c2ab2ccdf29360e163cb718
Bober <towermanxxl@gmail.com>
2025-12-05 16:05:31 +0100

    removed excessive event handling in eventPool

--- server/eventPool/EventPool.py
+++ server/eventPool/EventPool.py
@@ -1,64 +1,30 @@
+from server.server.eventPool import ConversationMsgEvent
 from .EventType import EventType
 from .events.Event import Event
-from typing import Dict, List
+from typing import Dict
 
 
 class EventPool:
     def __init__(self):
-        self.events: Dict[EventType, List[ Event ] ] = {}
+        self.events: Dict[EventType, Event ] = {
+            EventType.NEW_MSG_IN_CONVERSATION: ConversationMsgEvent(),
+            EventType.NEW_CONVERSATION: None
+        }
 
     async def notify_event(self, eventType: EventType, additionalEventInfo=None) -> bool:
         if not self.checkIfValidEventType(eventType):
             return False
         
-        for event in self.events[eventType]:
-            await event.notify(additionalEventInfo)
+        await self.events[eventType].notify(additionalEventInfo)
 
 
         return True
     
-    def addEventType(self, event_type: EventType) -> bool:
-        if self.checkIfValidEventType(event_type):
-            return False
-        
-        self.events[event_type] = []
-        return True
-    
-    def removeEventType(self, event_type: EventType) -> bool:
-        if not self.checkIfValidEventType(event_type):
-            return False
-        
-        self.events.pop(event_type)
-        return True
-    
-    def clearAllEvents(self) -> bool:
-        self.events = {}
-        return True
-    
-    def removeEvents(self, eventType: EventType) -> bool:
-        if not self.checkIfValidEventType(eventType):
-            return False
-        
-        self.events[eventType] = []
-        return True
-
     def checkIfValidEventType(self, event_type: EventType) -> bool:
         if event_type not in self.events.keys():
             return False
         else:
             return True
 
-    def registerEvent(self,event: Event) -> bool:
-        if not self.checkIfValidEventType(event.event_type):
-            return False
-        
-        self.events[event.event_type].append(event)
-        return True
-
-    def removeEvent(self,event: Event) -> bool:
-        if not self.checkIfValidEventType(event.event_type):
-            return False
-        
-        self.events[event.event_type].remove(event)
-        return True
+    
         
--- server/eventPool/__init__.py
+++ server/eventPool/__init__.py
@@ -1,7 +1,6 @@
 from .EventType import EventType
 from .EventPool import EventPool
 from .events.Event import Event
-from .events.ConversationMsgEvent import ConversationMsgEvent
 import server.globals as globals
 
 __all__ = ["EventType","EventPool","Event"]
@@ -10,13 +9,4 @@ __all__ = ["EventType","EventPool","Event"]
 
 
 def setupEventPool() -> None:
-    event_pool: EventPool = EventPool()
-
-
-    conversation_msg_event = ConversationMsgEvent()
-
-    event_pool.addEventType(EventType.NEW_MSG_IN_CONVERSATION)
-    event_pool.registerEvent(conversation_msg_event)
-
-    globals.eventPool = event_pool
-    globals.conversation_msg_event = conversation_msg_event
+    globals.eventPool = EventPool()
